29bc896a71bfe0594aecc38b3a20975d99f6d15c
Victoriia-Ripka <vikaripka@gmail.com>
2024-03-22 14:07:35 +0100

    changed structure dataset, updated forward_propagate, started backward_propagate_error

--- lab2/pattern_nn.py
+++ lab2/pattern_nn.py
@@ -24,11 +24,14 @@ def softmax(x):
     return exp_x / np.sum(exp_x, axis=0)
 
 class NeuralNetwork:
+    # n_hidden_layer, n_hidden_neurons
     def __init__(self, n_hidden, l_rate, n_epoch, filename, f, f_derivative):
         self.l_rate = l_rate
         self.n_epoch = n_epoch
+        self.f = f
+        self.f_derivative = f_derivative
 
-        count_of_data, n_inputs, n_outputs, data, codes = self.read_dataset(filename)
+        n_inputs, n_outputs, data, codes = self.read_dataset(filename)
 
         self.dataset_inputs = data
         self.dataset_outputs = codes
@@ -42,37 +45,46 @@ class NeuralNetwork:
         with open(filename, 'r') as file:
             lines = file.readlines()
 
-        count_of_data = int(lines[0])
-        count_of_inputs = int(lines[1])
-        count_of_outputs = int(lines[2])
+        count_of_inputs = int(lines[0])
+        count_of_outputs = int(lines[1])
         data = []
         codes = []
 
-        for i in range(4, len(lines), 3):
+        for i in range(3, len(lines), 3):
             data_line = lines[i].strip()
             code_line = lines[i + 1].strip()
 
             data.append([x for x in data_line.split(',')])
             codes.append(code_line)
 
-        return count_of_data, count_of_inputs, count_of_outputs, data, codes
+        return count_of_inputs, count_of_outputs, data, codes
 
 
-    def forward_propagate(self, data):
-        # вираховується зважена сума до прошарку
-        self.hidden_input = np.dot(data, self.weights_hidden_input) + self.hidden_bias
-        # вихід після функції активації
-        self.hidden_output = self.transfer(self.hidden_input)
-        # вираховується вихід з прошарку
-        self.output = np.dot(self.hidden_output, self.weights_hidden_output) + self.output_bias
-        return self.output
-    
+    def forward_propagate(self):
+        output = []
+        
+        for i in range(len(self.dataset_inputs)):
+            # вираховується зважена сума до прошарку
+            data_numeric = np.array(self.dataset_inputs[i], dtype=int)
+            self.hidden_input = np.dot(data_numeric, self.weights_hidden_input) + self.hidden_bias[0]
+            # вихід після функції активації
+            self.hidden_output = self.f(self.hidden_input)
+            # вираховується вихід з прошарку
+            result = np.dot(self.hidden_output, self.weights_hidden_output) + self.output_bias
+            output.append(result[0][0])
+        return output
+
 
     def backward_propagate_error(self, output):
+        print(output)
         # обрахування похибки для прихованого прошарку
-        d_output = np.subtract(self.dataset_outputs, output)
+        output_numeric = np.array(self.dataset_outputs, dtype=int)
+        d_output = np.subtract(output_numeric, output)
+
+        print(d_output)
+        print(self.weights_hidden_output.T)
         d_hidden_output = np.dot(d_output, self.weights_hidden_output.T)
-        d_hidden_input = d_hidden_output * self.transfer_derivative(self.hidden_output)
+        d_hidden_input = d_hidden_output * self.f_derivative(self.hidden_output)
         self.update_weights(d_output, d_hidden_input)
     
 
@@ -88,7 +100,7 @@ class NeuralNetwork:
 
     def train_network(self, error_threshold=0.00001):
         for epoch in range(self.n_epoch):
-            output = self.forward_propagate(self.dataset_inputs)
+            output = self.forward_propagate()
             self.backward_propagate_error(output)
 
             current_error = abs(float(output[0][0]) - float(self.dataset_outputs[0][0]))
@@ -106,6 +118,7 @@ n_epoch = 15000
 filename = "train.py"
 
 my_nn = NeuralNetwork(36, l_rate, n_epoch, filename, sigmoid, sigmoid_derivative)
+my_nn.train_network()
 # без прихованого шару
 # 1 прихований шар 36 нейронів
 # 1 прихований шар 72 нейрони
