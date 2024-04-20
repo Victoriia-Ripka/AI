88b3aea7cc231f4bfc4b30aaca355a60a8ec6b2f
Victoriia-Ripka <vikaripka@gmail.com>
2024-04-21 00:16:43 +0200

    .

--- lab4/index.py
+++ lab4/index.py
@@ -2,26 +2,42 @@ import numpy as np
 from matrix import R, Q
 
 class Agent:
-    def __init__(self, l_rate):
+    def __init__(self, start, end, l_rate):
+        self.aim = end
+        self.path = [start]
         self.R = R
         self.Q = Q
         self.l_rate = l_rate
 
 
-    def learn(self):
-        print(self.R[0])
+    def learn(self, shots):
 
+        for i in range(shots):
+            while self.path[-1] != self.aim:
+                indexes_to_move = np.where(self.R[self.path[-1]] == 0)[0]
+                next_index = np.random.choice(indexes_to_move)
+                self.path.append(next_index)
+                
+                
+                if i > 0:
+                    pass
 
+            print(self.path)
+            self.path = [self.path[0]]
+
+        
     def run(self):
         print(self.Q[0])
 
 
 def main():
     l_rate = 0.8
+    start = 1
+    end = 25
 
-    cat = Agent(l_rate)
-    cat.learn()
-    cat.run()
+    cat = Agent(start - 1, end - 1, l_rate)
+    cat.learn(1)
+    # cat.run()
 
 
 main()
