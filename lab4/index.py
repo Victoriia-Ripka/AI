6b74e886e3f967acc22aa497b0bc3945e4c26571
Victoriia-Ripka <vikaripka@gmail.com>
2024-04-21 17:20:32 +0200

    done

--- lab4/index.py
+++ lab4/index.py
@@ -18,25 +18,42 @@ class Agent:
     def update_memory(self):
         if len(self.path) > 2:
             # Отримати доступні дії для поточного стану
-            possible_steps = np.where(self.R[self.path[-1]] == 0)[0]  
+            possible_steps = np.where(self.R[self.path[-2]] >= 0)[0]  
+
             q_steps = []
             for i in range(len(possible_steps)):
-                q = self.Q[self.path[-1], possible_steps[i]]
+                q = self.Q[self.path[-2], possible_steps[i]]
                 q_steps.append(q)
+
             # Максимальна користь від переміщення у даний стан
             max_step_benefit = np.max(q_steps)
-            # Обрахунок за формулою
-            self.Q[self.path[-2], self.path[-1]] = self.R[self.path[-2], self.path[-1]] + self.l_rate * max_step_benefit
 
+            # Обрахунок за формулою
+            self.Q[self.path[-3], self.path[-2]] = self.R[self.path[-3], self.path[-2]] + self.l_rate * max_step_benefit
+            
 
     # Вибір дії на підставі матриці суміжності станів
     def choose_action(self):
         # Отримати доступні дії для поточного стану
-        possible_actions = np.where(self.R[self.path[-1]] == 0)[0] 
+        possible_actions = np.where(self.R[self.path[-1]] >= 0)[0] 
         next_index = np.random.choice(possible_actions) 
         return next_index
     
+
+    def choose_action_based_memory(self):
+        # Отримати доступні дії для поточного стану
+        possible_actions = np.where(self.R[self.path[-1]] >= 0)[0] 
+        # Знайти вагу переходів у кожен крок
+        q_steps = []
+        for i in range(len(possible_actions)):
+            q = self.Q[self.path[-1], possible_actions[i]]
+            q_steps.append(q)
+
+        # Обрати потрібний крок
+        index = np.argmax(q_steps) 
+        return possible_actions[index]
     
+
     # Переміщення в новий стан
     def move(self, action):
         self.path.append(action) 
@@ -52,22 +69,17 @@ class Agent:
                 self.move(next_step)
                 self.update_memory()
 
-            print(f"\n\nthe {iteration + 1} try: ")
-            print(f"Q matrix ")
-            for i in range(len(self.Q)):
-                print(self.Q[i])
+            print(f"INFO: {iteration + 1} try done")
 
 
-    # Метод для проходження поля на підставі досвіду
-    # треба рухатися на основі матриця пам'яті   
-    def run(self, start_state=0):
-        self.path = [start_state]
+    # Метод для проходження поля на підставі досвіду 
+    def run(self):
+        self.path = [self.start_state]
+
         while self.path[-1] != self.goal_state:
-            action = self.choose_action()
-            next_state = action
-            self.path.append(next_state)
-            self.move(next_state)
-            print(self.path)
+            next_step = self.choose_action_based_memory()
+            self.move(next_step)
+            
         return self.path
 
 
@@ -77,9 +89,18 @@ def main():
     end = 7
 
     cat = Agent(start - 1, end - 1, l_rate)
-    cat.train(3)
-    # print("the path after learning: ")
-    # print(cat.run())
+
+    print(f"Q matrix ")
+    for row in cat.Q:
+        print(" ".join(map(lambda x: f"{x}", row)))
+
+    cat.train(10)
+
+    print(f"Q matrix ")
+    for row in cat.Q:
+        print(" ".join(map(lambda x: f"{x}", row)))
+
+    print("\n\nthe path after learning: ", cat.run())
 
 
 main()
